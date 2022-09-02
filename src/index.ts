fa03f7e6a56527beddc0d3778704c32d6a11591f
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2022-09-02 12:46:55 +0200

    feat(types): Export ReadWriteOptions
    This type is useful when implementing a custom adapter in a downstream
    package that uses TypeScript. Previously, the "write()" method couldn't
    be written properly.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { Adapter } from './adapter.js'
+export { Adapter, ReadWriteOptions } from './adapter.js'
 export { MemoryAdapter } from './memory-adapter.js'
 export { DirectoryAdapter } from './directory-adapter.js'
